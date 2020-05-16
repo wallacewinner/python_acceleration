4475f3118ec49e7a95c0668250fdb14c2147f279
wallace winner <wallacewinner@gmail.com>
2020-05-16 19:20:51 -0300

    edit: adapt to pep8

--- python-5/main.py
+++ python-5/main.py
@@ -1,39 +1,84 @@
 from datetime import datetime
 
-records = [
-    {'source': '48-996355555', 'destination': '48-666666666', 'end': 1564610974, 'start': 1564610674},
-    {'source': '41-885633788', 'destination': '41-886383097', 'end': 1564506121, 'start': 1564504821},
-    {'source': '48-996383697', 'destination': '41-886383097', 'end': 1564630198, 'start': 1564629838},
-    {'source': '48-999999999', 'destination': '41-885633788', 'end': 1564697158, 'start': 1564696258},
-    {'source': '41-833333333', 'destination': '41-885633788', 'end': 1564707276, 'start': 1564704317},
-    {'source': '41-886383097', 'destination': '48-996384099', 'end': 1564505621, 'start': 1564504821},
-    {'source': '48-999999999', 'destination': '48-996383697', 'end': 1564505721, 'start': 1564504821},
-    {'source': '41-885633788', 'destination': '48-996384099', 'end': 1564505721, 'start': 1564504821},
-    {'source': '48-996355555', 'destination': '48-996383697', 'end': 1564505821, 'start': 1564504821},
-    {'source': '48-999999999', 'destination': '41-886383097', 'end': 1564610750, 'start': 1564610150},
-    {'source': '48-996383697', 'destination': '41-885633788', 'end': 1564505021, 'start': 1564504821},
-    {'source': '48-996383697', 'destination': '41-885633788', 'end': 1564627800, 'start': 1564626000}
-]
+records = [{
+    'source': '48-996355555',
+    'destination': '48-666666666',
+    'end': 1564610974,
+    'start': 1564610674}, {
+    'source': '41-885633788',
+    'destination': '41-886383097',
+    'end': 1564506121,
+    'start': 1564504821}, {
+    'source': '48-996383697',
+    'destination': '41-886383097',
+    'end': 1564630198,
+    'start': 1564629838}, {
+    'source': '48-999999999',
+    'destination': '41-885633788',
+    'end': 1564697158,
+    'start': 1564696258}, {
+    'source': '41-833333333',
+    'destination': '41-885633788',
+    'end': 1564707276,
+    'start': 1564704317}, {
+    'source': '41-886383097',
+    'destination': '48-996384099',
+    'end': 1564505621,
+    'start': 1564504821}, {
+    'source': '48-999999999',
+    'destination': '48-996383697',
+    'end': 1564505721,
+    'start': 1564504821}, {
+    'source': '41-885633788',
+    'destination': '48-996384099',
+    'end': 1564505721,
+    'start': 1564504821}, {
+    'source': '48-996355555',
+    'destination': '48-996383697',
+    'end': 1564505821,
+    'start': 1564504821}, {
+    'source': '48-999999999',
+    'destination': '41-886383097',
+    'end': 1564610750,
+    'start': 1564610150}, {
+    'source': '48-996383697',
+    'destination': '41-885633788',
+    'end': 1564505021,
+    'start': 1564504821}, {
+    'source': '48-996383697',
+    'destination': '41-885633788',
+    'end': 1564627800,
+    'start': 1564626000}]
 
-call_tax = 0.39
-call_tax_minute = 0.09
+
+call_tax = 0.39  # fixed tax all calls
+tax_minute = 0.09  # tax to calls during 6 and 22
+call_response = []  # array to save the result
+
 
 def classify_by_phone_number(records):
     for record in records:
-        total_call = calculate_duration(record['start'], record['end'])   
-        print(total_call)
+        # cost of the it call
+        total_call = calculate_duration(record['start'], record['end'])
+        # save the result on array
+        call_response.append({'source': record['source'], 'total': total_call})
+
+    print(call_response)
     pass
 
+
 def calculate_duration(start, end):
+    # convert the time
     start_convert = datetime.fromtimestamp(start)
     end_convert = datetime.fromtimestamp(end)
+    # calculate the duration of call
     duration = end_convert - start_convert
-
-    if start_convert.hour > 6 and start_convert.hour < 22:    
-        result = ( ( int(duration.total_seconds() / 60) ) * call_tax_minute  + call_tax)
+    # define the time that call starts
+    if start_convert.hour > 6 and start_convert.hour < 22:
+        result = ((int(duration.total_seconds() / 60)) * tax_minute + call_tax)
     else:
-        result = ( ( int(duration.total_seconds() / 60) ) * 0  + call_tax )
-
+        result = ((int(duration.total_seconds() / 60)) * 0 + call_tax)
+    # round the result for two decimal points
     return round(result, 2)
 
 
