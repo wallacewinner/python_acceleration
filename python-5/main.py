f0f20e4e4744a1302b3377e97a4399133e7f5d3e
wallace winner <wallacewinner@gmail.com>
2020-05-16 18:19:57 -0300

    edit: method time duration

---
+++ python-5/main.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+
+records = [
+    {'source': '48-996355555', 'destination': '48-666666666', 'end': 1564610974, 'start': 1564610674},
+    {'source': '41-885633788', 'destination': '41-886383097', 'end': 1564506121, 'start': 1564504821},
+    {'source': '48-996383697', 'destination': '41-886383097', 'end': 1564630198, 'start': 1564629838},
+    {'source': '48-999999999', 'destination': '41-885633788', 'end': 1564697158, 'start': 1564696258},
+    {'source': '41-833333333', 'destination': '41-885633788', 'end': 1564707276, 'start': 1564704317},
+    {'source': '41-886383097', 'destination': '48-996384099', 'end': 1564505621, 'start': 1564504821},
+    {'source': '48-999999999', 'destination': '48-996383697', 'end': 1564505721, 'start': 1564504821},
+    {'source': '41-885633788', 'destination': '48-996384099', 'end': 1564505721, 'start': 1564504821},
+    {'source': '48-996355555', 'destination': '48-996383697', 'end': 1564505821, 'start': 1564504821},
+    {'source': '48-999999999', 'destination': '41-886383097', 'end': 1564610750, 'start': 1564610150},
+    {'source': '48-996383697', 'destination': '41-885633788', 'end': 1564505021, 'start': 1564504821},
+    {'source': '48-996383697', 'destination': '41-885633788', 'end': 1564627800, 'start': 1564626000}
+]
+
+
+def classify_by_phone_number(records):
+    for record in records:
+        result = calculate_duration(record['start'], record['end'])   
+        #print(result)
+    pass
+
+def calculate_duration(start, end):
+    start_convert = datetime.fromtimestamp(start)
+    end_convert = datetime.fromtimestamp(end)
+
+    duration = end_convert - start_convert
+
+    if start_convert.hour > 6 and start_convert.hour < 22:  
+        print('diurno-->')  
+        print((int(start_convert.hour)*0.09) + 0.39)
+    else:
+        print('noturno-->')
+        print((int(start_convert.hour)* 0 )+0.39)
+    
+    return (int(duration.total_seconds()/60))
+
+
+(classify_by_phone_number(records))
