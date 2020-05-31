4f660362e9efede16abed843aeafa466cbba054b
wallace winner <wallacewinner@gmail.com>
2020-05-30 21:55:54 -0300

    add: test temp

--- python-7/test_main.py
+++ python-7/test_main.py
@@ -1,5 +1,13 @@
+import pytest
 from main import get_temperature
 
 
-def test_get_temperature_by_lat_lng():
-    raise NotImplementedError
+def get_temperature_by_lat_lng(temperature):
+    if not temperature:
+        return
+    return int((temperature - 32) * 5.0 / 9.0)
+
+def teste_temperature_is_sixteen():
+    temperature = 62
+    resp = get_temperature_by_lat_lng(temperature)
+    assert resp == 16
