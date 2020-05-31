49c9fda63f073270ba577e99146c81139656be5b
wallace winner <wallacewinner@gmail.com>
2020-05-30 22:07:55 -0300

    update: using parametrize

--- python-7/test_main.py
+++ python-7/test_main.py
@@ -1,13 +1,12 @@
 import pytest
 from main import get_temperature
 
-
 def get_temperature_by_lat_lng(temperature):
     if not temperature:
         return
     return int((temperature - 32) * 5.0 / 9.0)
 
-def teste_temperature_is_sixteen():
-    temperature = 62
+@pytest.mark.parametrize('temperature, expected', [(62, 16)])
+def test_temperature_is_sixteen(temperature, expected):
     resp = get_temperature_by_lat_lng(temperature)
-    assert resp == 16
+    assert resp == expected
