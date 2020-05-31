25dc98f82ac11dfe44a84462fdd4286ad4407135
wallace winner <wallacewinner@gmail.com>
2020-05-30 22:28:18 -0300

    fix: pip8

--- python-7/test_main.py
+++ python-7/test_main.py
@@ -1,5 +1,4 @@
 import pytest
-from main import get_temperature
 
 temperature_positive = [
     (62, 16),
@@ -11,20 +10,24 @@ temperature_negative = [
     (33, 0)
 ]
 
+
 def get_temperature_by_lat_lng(temperature):
     if not temperature:
         return
     return int((temperature - 32) * 5.0 / 9.0)
+
 
 @pytest.mark.parametrize('temperature, expected', temperature_positive)
 def test_temperature_is_positive(temperature, expected):
     resp = get_temperature_by_lat_lng(temperature)
     assert resp == expected
 
+
 @pytest.mark.parametrize('temperature, expected', temperature_negative)
 def test_temperature_is_negative(temperature, expected):
     resp = get_temperature_by_lat_lng(temperature)
     assert resp == expected
+
 
 @pytest.mark.parametrize('temperature, expected', [('', None)])
 def test_temperature_is_error(temperature, expected):
