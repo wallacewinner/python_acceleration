c489dcc1ad97de2611b6458120f5755e0d7312c1
wallace winner <wallacewinner@gmail.com>
2020-06-06 17:44:38 -0300

    update: encoded, decode method

--- python-8/main.py
+++ python-8/main.py
@@ -3,8 +3,15 @@ import jwt
 
 def create_token(data, secret):
     encoded = jwt.encode(data, secret, algorithm='HS256')
-    return encoded
+
+    if verify_signature(encoded):
+        return encoded
+    else:
+        return False
 
 
 def verify_signature(token):
-    raise NotImplementedError()
+    try:
+        return jwt.decode(token, 'acelera', algorithms=['HS256'])
+    except jwt.exceptions.InvalidSignatureError:
+        return {"error": 2}
