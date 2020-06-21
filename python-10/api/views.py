6040e957cc85489be034c5177dee7f56dcdba74b
wallace winner <wallacewinner@gmail.com>
2020-06-21 12:30:06 -0300

    delete: lambda project

--- python-10/api/views.py
+++ python-10/api/views.py
@@ -1,5 +1,13 @@
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from collections import Counter
 
+
+@api_view(['POST'])
 def lambda_function(request):
-    raise NotImplementedError
+    data = request.data.get('question')
 
+    lista = list(Counter(data).elements())
+    list_ordenada = sorted(lista, key=lista.count, reverse=True)
+    return Response({'solution': list_ordenada})
