c7c66fdbc6896a4c5ec353a8169a3897c68dd8f2
wallace winner <wallacewinner@gmail.com>
2020-07-05 14:26:11 -0300

    add: response

--- python-11/api/models.py
+++ python-11/api/models.py
@@ -65,4 +65,4 @@ class Event(models.Model):
 
     class Meta:
         ordering = ['date']
-
+        
--- python-11/main.py
+++ python-11/main.py
@@ -3,40 +3,39 @@ from datetime import datetime, timedelta
 
 
 def get_active_users() -> User:
-    now = datetime.today()
-    active_users = User.objects.filter(
-        last_login_gte = (
-            now - timedelta(10)
+    active = User.objects.filter(
+        last_login__gte = (
+            datetime.today() - timedelta(10)
         )
     )
-    return active_users
+    return active
 
 
 def get_amount_users() -> User:
-    all_users = User.objects.count()
-    return all_users
+    users = User.objects.count()
+    return users
 
 
 def get_admin_users() -> User:
-    """Traga todos os usuarios com grupo = 'admin"""
-    raise NotImplementedError
+   admins = User.objects.filter(group__name='admin')
+   return admins
 
 
 def get_all_debug_events() -> Event:
-    """Traga todos os eventos com tipo debug"""
-    raise NotImplementedError
+    events = Event.objects.filter(level='debug')
+    return events
 
 
 def get_all_critical_events_by_user(agent) -> Event:
-    """Traga todos os eventos do tipo critico de um usuário específico"""
-    raise NotImplementedError
+    critical = Event.objects.filter(level='critical', agent=agent)
+    return critical
 
 
 def get_all_agents_by_user(username) -> Agent:
-    """Traga todos os agentes de associados a um usuário pelo nome do usuário"""
-    raise NotImplementedError
+    agent = Agent.objects.filter(user__name=username)
+    return agent
 
 
 def get_all_events_by_group() -> Group:
-    """Traga todos os grupos que contenham alguem que possua um agente que possuem eventos do tipo information"""
-    raise NotImplementedError
+   group = Group.objects.filter(user__agent__event__level = 'information')
+   return group
